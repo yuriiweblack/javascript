2a6315bb203b0695a07f31274597005015cf38a8
yuriiweblack <yuriiwebblack@gmail.com>
2023-05-03 18:46:41 +0300

    loops 100%+change

--- js/loops.js
+++ js/loops.js
@@ -24,15 +24,29 @@ for(let a = 0; a <= 3; a++){
  console.log(a);
 };
 
-// while (true) {
-//  let value_1 = prompt('Enter a number','');
+while (true) {
+ let value_1 = prompt('Enter a number','');
 
-//  if (value_1 === null){
-//   break;
-//  }
+ if (value_1 === null){
+  break;
+ }
 
-//  console.log(value_1)
-// }
+ switch (value_1) {
+  case "sun":
+   alert("Сонце");
+   break;
+  case 'rain':
+   alert("Дощ");
+   break;
+  case 5:
+   alert("Less...");
+   break;
+  default:
+   alert(" I don't know this value!");
+ }
+
+ console.log(value_1)
+}
 
 // continue - повертає до умови циклу
 
@@ -43,12 +57,22 @@ if (b == 2) {
 }
 
  console.log(b);
+ 
 }
 
 let fruits = ["Orange", "Bannana", "Apple", "Mango", "Kiwi"]
+let new_fruits = []
 
 for (let iter = 0; iter < fruits.length; iter++){
  console.log(fruits[iter]);
+ alert(fruits[iter])
+ new_fruits.push(fruits[iter])
+
 }
 
+alert(`All list - ${new_fruits}`)
 
+
+// function name(params) {
+ 
+// }
